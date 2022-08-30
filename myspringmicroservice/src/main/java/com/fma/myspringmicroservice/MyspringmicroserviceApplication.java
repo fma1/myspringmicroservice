78f2999cef6fc19d1db2e3c5fcf6b033dbae0b3d
Fred Ma <fredma1234@gmail.com>
2022-08-30 16:43:06 -0400

    Initial commit from Spring Initialzr with Spring Web, Spring Data JDBC and H2 Database

---
+++ myspringmicroservice/src/main/java/com/fma/myspringmicroservice/MyspringmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fma.myspringmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringmicroserviceApplication.class, args);
+	}
+
+}
---
+++ myspringmicroservice/src/test/java/com/fma/myspringmicroservice/MyspringmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fma.myspringmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
